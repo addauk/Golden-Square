7b34cf9abc020c882a587f240fc94f206bd208af
addauk <addauk@gmail.com>
2023-01-24 11:01:02 +0000

    Added .ordered to messages as a reminder that rspec doesn't particularly care for order otherwise

--- mocking_bites/testing_terminal_io/spec/interactive_calculator_spec.rb
+++ mocking_bites/testing_terminal_io/spec/interactive_calculator_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe InteractiveCalculator do
     
     io = double :io
 
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("4")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("3")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("4 - 3 = 1")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("4").ordered
+    expect(io).to receive(:puts).with("Please enter another number").ordered
+    expect(io).to receive(:gets).and_return("3").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("4 - 3 = 1").ordered
 
     interactive_calculator = InteractiveCalculator.new(io)
     interactive_calculator.run
